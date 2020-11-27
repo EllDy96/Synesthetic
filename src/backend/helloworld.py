690df1ff0dc3dc6c83e01283f1fcba0957274779
Michele Perrone <perrone.michele@outlook.com>
2020-11-27 15:01:10 +0100

    updated README

--- src/backend/helloworld.py
+++ src/backend/helloworld.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+# Da js facciamo richieste HTTP
+# Richiesta POST su server
+# qua potrete trovare un esempio di un semplice HTTP 
+# server scritto in Python che accetta chiamate GET e POST da un client che fa richieste su 
+# http://localhost:8080. Quindi voi dovreste scrivere la parte client come sapete fare e, 
+# quando avrete bisogno di fare un chiamata al server (per esempio per chiedere di processare un segnale), 
+# fare da Javascript una chiamata POST al server sulla porta 8080 contenente i dati che il server necessita 
+# e aspettare la sua risposta (il segnale processato).
