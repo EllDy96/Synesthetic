53264aad7ec0e57d29bbe05963e0d564494950a8
ElDy96 <45422271+ElDy96@users.noreply.github.com>
2020-12-24 16:52:30 +0100

    Prova

--- src/frontend/oscilloscope.js
+++ src/frontend/oscilloscope.js
@@ -1,63 +1,103 @@
-c = new AudioContext();
-o = c.createOscillator();
-canvas = document.querySelector("canvas");
-ctx = canvas.getContext("2d");
-a = c.createAnalyser();
+ 
+/* referance: https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API */
+//var audioCtx = new (window.AudioContext || window.webkitAudioContext);
+//const AudioContext = require("web-audio-engine").StreamAudioContext;
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-var myAudio = document.querySelector("audio");
-var source = c.createMediaElementSource(myAudio);
+///*  */
+
+c = new AudioContext(); 
+o = c.createOscillator(); 
+canvas = document.querySelector("canvas"); 
+ctx = canvas.getContext("2D"); 
+a = c.createAnalyser(); 
+
+
+
+var audioElement = document.querySelector("audio");
+audioElement.crossOrigin = "anonymous";
+var source = c.createMediaElementSource(audioElement);
+// connecting the source to the AudioC. destination  just to play it 
+
+source.connect(c.destination);
+
+// select our play button
+const playButton = document.querySelector('button');
+// Toggling the button state accordingly to the play or pause state
+playButton.addEventListener('click', function() {
+
+    // check if context is in suspended state (autoplay policy)
+    if (c.state === 'suspended') {
+        c.resume();
+    }
+
+    // play or pause track depending on state
+    if (this.dataset.playing === 'false') {
+        audioElement.play();
+        this.dataset.playing = 'true';
+    } else if (this.dataset.playing === 'true') {
+        audioElement.pause();
+        this.dataset.playing = 'false';
+    }
+
+}, false);
+
+// we have to check when the audioElement is finished and switch the button state again
+audioElement.addEventListener('ended', () => {
+  playButton.dataset.playing = 'false';
+}, false);
 // o.connect(source);
 // o.start();
 
 // To extract data from your audio source, you need an AnalyserNode
-var analyser = c.createAnalyser();
-// i have to connet this node to the source
-source.connect(analyser);
-// and then connect my analyser to the Audio destination
-analyser.connect(c.destination);
+// var analyser = c.createAnalyser();
+// // i have to connet this node to the source
+// source.connect(analyser);
+// // and then connect my analyser to the Audio destination
+// analyser.connect(c.destination);
 
-// to capture frequency data of the audio source
-var bufferLength = analyser.frequencyBinCount;
-var dataFreqsArray = new Float32Array(bufferLength); // Float32Array
-void analyser.getFloatFrequencyData(dataFreqsArray); // fill the Float32Array with data returned from getFloatFrequencyData()
-//The getFloatFrequencyData() method of the AnalyserNode Interface copies the current frequency data into a Float32Array array passed into it.Each item in the array represents the decibel value for a specific frequency.
+// // to capture frequency data of the audio source
+// var bufferLength = analyser.frequencyBinCount;
+// var dataFreqsArray = new Float32Array(bufferLength); // Float32Array
+// void analyser.getFloatFrequencyData(dataFreqsArray); // fill the Float32Array with data returned from getFloatFrequencyData()
+// //The getFloatFrequencyData() method of the AnalyserNode Interface copies the current frequency data into a Float32Array array passed into it.Each item in the array represents the decibel value for a specific frequency.
 
-// create the canvas
-ctx.clearRect(0, 0, 100, 100);
+// // create the canvas
+// ctx.clearRect(0, 0, 100, 100);
 
-function draw() {
-  // to keep repeating the drawing function once it has been started
-  var drawVisual = requestAnimationFrame(draw);
-  // get the time domain data and copy to the array
-  analyser.getByteTimeDomainData(dataFreqsArray);
-  //fill the canvas with a starting color
-  ctx.fillStyle = "rgb(200,200,200)";
-  ctx.fillRect(0, 0, width, height);
+// function draw() {
+//   // to keep repeating the drawing function once it has been started
+//   var drawVisual = requestAnimationFrame(draw);
+//   // get the time domain data and copy to the array
+//   analyser.getByteTimeDomainData(dataFreqsArray);
+//   //fill the canvas with a starting color
+//   ctx.fillStyle = "rgb(200,200,200)";
+//   ctx.fillRect(0, 0, width, height);
 
-  // seting some parameters of the line to draw
-  ctx.lineWight = 2;
-  ctx.strokeStyle = "rgb(0,0,0)";
-  ctx.beginPath();
-  //Determine the width of each segment of the line to be drawn by dividing the canvas width by the array length
-  var sliceWidth = (width * 1.0) / bufferLength;
-  var x = 0;
+//   // seting some parameters of the line to draw
+//   ctx.lineWight = 2;
+//   ctx.strokeStyle = "rgb(0,0,0)";
+//   ctx.beginPath();
+//   //Determine the width of each segment of the line to be drawn by dividing the canvas width by the array length
+//   var sliceWidth = (width * 1.0) / bufferLength;
+//   var x = 0;
 
-  //Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer at a certain height based on the data point value form the array, then moving the line across to the place where the next wave segment should be drawn:
-  for (var i = 0; i < bufferLength; i++) {
-    var v = dataArray[i] / 128.0;
-    var y = (v * HEIGHT) / 2;
+//   //Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer at a certain height based on the data point value form the array, then moving the line across to the place where the next wave segment should be drawn:
+//   for (var i = 0; i < bufferLength; i++) {
+//     var v = dataArray[i] / 128.0;
+//     var y = (v * HEIGHT) / 2;
 
-    if (i === 0) {
-      cx.moveTo(x, y);
-    } else {
-      ctx.lineTo(x, y);
-    }
+//     if (i === 0) {
+//       cx.moveTo(x, y);
+//     } else {
+//       ctx.lineTo(x, y);
+//     }
 
-    x += sliceWidth;
-  }
-  ctx.lineTo(canvas.width, canvas.height / 2);
-  //draw the stroke we've defined
-  ctx.stroke();
-}
+//     x += sliceWidth;
+//   }
+//   ctx.lineTo(canvas.width, canvas.height / 2);
+//   //draw the stroke we've defined
+//   ctx.stroke();
+// }
 
-draw();
+// draw();
