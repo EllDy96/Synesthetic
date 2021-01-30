f70e057f89f404990953d44a07b09b47747fd87e
Jacopo Barzon <jacopo.barz@gmail.com>
2021-01-30 16:19:18 +0100

    double circle lift inside draw

--- src/static/animation_pataS.js
+++ src/static/animation_pataS.js
@@ -1,6 +1,7 @@
 let g 
 let dim = 120
-
+let n_ritmi = 2;
+let r
 function setup() {
   createCanvas(720, 400);
   noStroke();
@@ -8,20 +9,43 @@ function setup() {
   // the default upper left corner
   rectMode(CENTER);
   g = height + 25;
+  r = height + 25;
 }
 
 function draw() {
   background(143);
+  push();
+  if (n_ritmi>=1){
   if (g >= height/2){
-    g = g - 8
+    g = g - 2
     //traslate(0,g);
   }
   if (g < height/2){
     //translate(0,g)
-    dim = dim -4;
+    dim = dim -1;
   } 
   fill('rgb(255,183,255)');
   circle(width/3, g ,dim);
   if(dim==0)
     noLoop();
+}
+  pop();
+  
+  push();
+  if (n_ritmi>=2){
+  if (r >= height/2){
+     r = r - 8
+    //traslate(0,g);
+  }
+  if (r < height/2){
+    //translate(0,g)
+    dim = dim -2;
+  } 
+  fill('rgb(5,18,255)');
+  circle(width-55, r ,dim);
+  if(dim==0)
+    noLoop();
+}
+  pop();
+  
 }
