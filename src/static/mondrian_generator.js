69fe402ab91a688a8409dedecc0f340003e31a84
Michele Perrone <perrone.michele@outlook.com>
2021-02-05 17:58:54 +0100

    Fix mondrian generator lerping

--- src/static/mondrian_generator.js
+++ src/static/mondrian_generator.js
@@ -1,20 +1,22 @@
 var color_palette = 
 [
-    "green",
+    "#d24632",
     "#f5e132",
     "#3278aa",
     "#ffffff",
+    "#ffffff"
 ];
 
 let x,y,dim1,dim2, b1, b2, numblocks;
 let c1,c2,c3,c4,k,s;
 var blocks= []; //array containing the blocks
-var sizes = [150,200,250, 300];
+var sizes = [150,200,250, 300]; // old: [50, 100, 150, 200];
 var rnd_color;
 var c_;
 
-function setup() {
-  createCanvas(800, 800);
+function setup() 
+{
+  createCanvas(windowWidth-20, windowHeight-20);
   strokeWeight(10);
   var i = 0;
   var x = 0;
@@ -23,21 +25,21 @@ function setup() {
   var currHeight = random(sizes);
   
   while (y < height) 
+  {
+    x = 0;
+    while (x <= width) 
     {
-        x = 0;
-        while (x <= width) 
-        {
-            // Choose randomly a color from the palette
-            rnd_color = random(color_palette);
-          
-            blocks[i] = new Block(x, y, currWidth, currHeight, rnd_color);
-            i+=1;
-            x = x + currWidth;
-            currWidth = random(sizes);
-        }
-        y = y + currHeight;
-        currHeight = random(sizes);
+        // Choose randomly a color from the palette
+        rnd_color = random(color_palette);
+      
+        blocks[i] = new Block(x, y, currWidth, currHeight, rnd_color);
+        i+=1;
+        x = x + currWidth;
+        currWidth = random(sizes);
     }
+    y = y + currHeight;
+    currHeight = random(sizes);
+  }
   
   //still setup bruh
   c1 = color(204, 102, 0); //orange
@@ -49,22 +51,26 @@ function setup() {
   s= 0;
 }
 
-function draw() {
-  background(220);
-  for(let i = 0; i < blocks.length; i++){
+function draw() 
+{
+  for(let i = 0; i < blocks.length; i++)
+  {
     blocks[i].display(blocks[i].color);
   }
   
-  s = s + 0.3;
+  s = s + 0.1;
   k = cos(s)
   setGradient(s,k);
 }
 
-function setGradient(s,k){
+function setGradient(s,k)
+{
   push()
-  for (i = 0; i<blocks.length; i++){
-    if(blocks[i].color == color_palette[0]){
-      let c_ = blocks[i].color;
+  for (i = 0; i<blocks.length; i++)
+  {
+    if(blocks[i].color == color_palette[0])
+    {
+      let c_ = color(blocks[i].color);
       let c = lerpColor(c1,c_,k);
       blocks[i].display(c);
     }
@@ -76,7 +82,8 @@ function setGradient(s,k){
 
 class Block {
   //constructor
-  constructor(x, y, dim_1, dim_2, color){
+  constructor(x, y, dim_1, dim_2, color)
+  {
     this.x= x;
     this.y = y;
     this.dim1 = dim_1;
@@ -84,7 +91,8 @@ class Block {
     this.color = color;
   }
   //functionalities
-  display(color_pass){
+  display(color_pass)
+  {
     fill(color_pass)
     rect(this.x,this.y, this.dim1, this.dim2)
   }
