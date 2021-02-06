ab01209acb1a45f356d9412ba68087ded768640b
Jacopo Barzon <jacopo.barz@gmail.com>
2021-02-06 16:30:07 +0100

    updated mondrian generator

--- src/static/mondrian_generator.js
+++ src/static/mondrian_generator.js
@@ -10,37 +10,58 @@ var color_palette =
 let x,y,dim1,dim2, b1, b2, numblocks;
 let c1,c2,c3,c4,k,s;
 var blocks= []; //array containing the blocks
-var sizes = [150,200,250, 300]; // old: [50, 100, 150, 200];
+var sizes = [50, 150, 200, 250, 300, 350, 400]; // old: [50, 100, 150, 200];
 var rnd_color;
 var c_;
+var curr_block;
 
 function setup() 
 {
-  createCanvas(windowWidth-20, windowHeight-20);
+  //createCanvas(windowWidth-20, windowHeight-20);
+  createCanvas(800, 800);
   strokeWeight(10);
   var i = 0;
   var x = 0;
   var y = 0;
   var currWidth = random(sizes);
   var currHeight = random(sizes);
-  
-  while (y < height) 
+
+if(Math.random()<0.5){
+  while (y + currHeight < height) 
   {
-    x = 0;
-    while (x <= width) 
-    {
-        // Choose randomly a color from the palette
-        rnd_color = random(color_palette);
-      
-        blocks[i] = new Block(x, y, currWidth, currHeight, rnd_color);
-        i+=1;
-        x = x + currWidth;
-        currWidth = random(sizes);
-    }
+    fillOnRows(y, currHeight)
     y = y + currHeight;
     currHeight = random(sizes);
   }
-  
+  fillOnRows(y, height-y)
+}else
+{
+ while (x + currWidth < width)
+ {
+   fillOnColumns(x, currWidth)
+   x = x + currWidth;
+   currWidth = random(sizes)
+ }
+ fillOnColumns(x, width-x)
+}
+
+//diagonale principale ->primi n mondrian con dimensioni random
+// for (i=0; x<width && y<height; i++){
+//   rnd_color = random(color_palette);
+//   blocks[i] = new Block(x,y, currWidth, currHeight, rnd_color);
+//   x = x + currWidth;
+//   y = y + currHeight;
+//   currWidth=random(sizes);
+//   currHeight=random(sizes);
+// }
+
+
+
+
+
+
+
+
   //still setup bruh
   c1 = color(204, 102, 0); //orange
   c2 = color(0, 102, 153); //blue
@@ -89,6 +110,7 @@ class Block {
     this.dim1 = dim_1;
     this.dim2 = dim_2;
     this.color = color;
+    this.area = (this.dim1*this.dim2);
   }
   //functionalities
   display(color_pass)
@@ -96,5 +118,41 @@ class Block {
     fill(color_pass)
     rect(this.x,this.y, this.dim1, this.dim2)
   }
+  
+}
+
+function fillOnRows(y, currHeight)
+{
+  currWidth = random(sizes);
+  x = 0;
+  while (x + currWidth < width) 
+  {
+      rnd_color = random(color_palette);
+      curr_block = new Block(x, y, currWidth, currHeight, rnd_color);
+      blocks.push(curr_block)
+      x = x + currWidth;
+      currWidth = random(sizes);
+  }
+ 
+  curr_block = new Block(x, y, width - x, currHeight, rnd_color);
+  blocks.push(curr_block)
+  
+}
+
+function fillOnColumns(x, currWidth)
+{
+  currHeight = random(sizes);
+  y = 0;
+  while (y + currHeight < height) 
+  {
+      rnd_color = random(color_palette);
+      curr_block = new Block(x, y, currWidth, currHeight, rnd_color);
+      blocks.push(curr_block)
+      y = y + currHeight;
+      currHeight = random(sizes);
+  }
+ 
+  curr_block = new Block(x, y, currWidth, height - y, rnd_color);
+  blocks.push(curr_block)
   
 }
