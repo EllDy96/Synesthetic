5c2e42580c99d0701955b746eb1db2e2674a99c4
Jacopo Barzon <jacopo.barz@gmail.com>
2021-02-15 16:13:49 +0100

    updated flask server

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,6 +1,6 @@
 import os
-from python_modules import periodicity_finder as pf
-from python_modules import tempodetection as td
+#from python_modules import periodicity_finder as pf
+from python_modules import tempo_detection as td
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 from werkzeug.utils import secure_filename
 import librosa 
@@ -11,7 +11,7 @@ import numpy as np
 app= Flask(__name__)#, template_folder=template_dir)
 app.config['MAX_CONTENT_LENGTH']=20*1024*1024 #fix the max size of the uploaded files at 20MB
 app.config['UPLOAD_EXTENSIONS']=['.mp3', '.wav', '.flac']
-app.config['UPLOAD_PATH']='uploaded_tracks'
+app.config['UPLOAD_PATH']='templates/assets/'
 
 @app.route('/')         #collegamento tra ('percorso url') e ...                        
 def upload():           #... funzione view
@@ -30,16 +30,16 @@ def upload_file():
  #  print("just an index,", x)
  #  pulses = td.generate_polyrhythm_impulse_train()
    track_path = os.path.join(app.config['UPLOAD_PATH'], filename)
-   [x, Fs]=librosa.load(track_path, sr=None)
-   [nov_x, Fs_nov_x] = td.compute_novelty_complex(x, Fs, N=512, H=256)
+   #[x, Fs]=librosa.load(track_path, sr=None)
+   #[nov_x, Fs_nov_x] = td.compute_novelty_complex(x, Fs, N=512, H=256)
 
    #td.plot_signal(nov_x, Fs_nov_x, 'Complex novelty function of the track', 'C2')
-
-   periods = td.periodicities_lookup(nov_x, EPS = 5, verbose=False)
-   print("Number of periodicities found in the signal:", len(periods))
-   for period in periods:
-      print("periodicity_len: %i, periodicity_offset: %i, periodicity_consecutive_counts: %i" % (period[0], period[1], period[2]) )
-   return render_template('oscilloscope.html', song_name=filename)
+   td.driver_function(track_path, 'templates/assets')
+   #periods = td.periodicities_lookup(nov_x, EPS = 5, verbose=False)
+   #print("Number of periodicities found in the signal:", len(periods))
+   #for period in periods:
+   #   print("periodicity_len: %i, periodicity_offset: %i, periodicity_consecutive_counts: %i" % (period[0], period[1], period[2]) )
+   return render_template('mondrian_animation.html')
 
 @app.route('/ <filename>')      #new method returns the track (uploaded in 'uploaded_tracks' folder)
 def send_track(filename):
@@ -49,14 +49,3 @@ def send_track(filename):
 def canvas():
    return render_template ('metronome.html')   
 
-#@app.route('/admin')            #just an example on how redirecting works in Flask(un utente non può finire su pagina /admin)
-#def admin():
-#    return redirect(url_for("home"))
-
-#@app.route('/<name>')           #just a simple example of extracting url stuff and write it into web page
-#def user(name):
-#    return f"Hello {name}!"
-
-#@app.route('/<name>')            #render an html template. In order to do that, create a "template" folder in the same directory of the current flask.py file
-#def page(name):                  #passing html template a list, then use python inside html
-#    return render_template("index.html", content=["Francesco Bruschi","Mauro Molinari", "Vincenzo Rana"], title=name) 
